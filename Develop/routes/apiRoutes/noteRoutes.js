9a315dea948f89ae0541a475e8ad7b1336c93f71
Orlando Lopez <94086814+Olopez92084@users.noreply.github.com>
2022-02-13 19:57:23 -0500

    Deployment debugging

--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -4,12 +4,12 @@ const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const path = require("path");
 
-// GET /api/notes
+// GET api, notes
 router.get("/notes", (req, res) => {console.log({ data });
   res.json(data);
 });
 
-// DELETE /api/notes/:id
+// DELETE api, notes/:id
 // get one specific note, req.body.id, readfile db.json, find entry that matches that id
 router.delete("/notes/:id", (req, res) => {
     data = data.filter((el) => el.id !== req.params.id);
